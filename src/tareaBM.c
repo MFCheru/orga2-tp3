78bd67c1f1a2dcd4c60722ae628bfbe18c08bec4
MFCheru <cheru.mf@gmail.com>
2019-03-18 03:34:38 -0300

    Agrego el codigo de las tareas

---
+++ src/tareaBM.c
@@ -0,0 +1,30 @@
+/* ** por compatibilidad se omiten tildes **
+================================================================================
+ TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+================================================================================
+*/
+
+#include "colors.h"
+#include "defines.h"
+#include "game.h"
+#include "syscall.h"
+#include "i386.h"
+
+void task(int x_target, int y_target) {
+    /* Tarea de minero del jugador B */
+
+    uint pos = syscall_posicion(-1);
+    int x = pos & 0xFF;
+    int y = pos >> 8;
+
+    int i;
+    for(i=x; i > x_target; i--)
+        syscall_mover(IZQ);
+
+    for(i=y; i > y_target; i--)
+        syscall_mover(ARR);
+
+    while(1) {syscall_cavar();}
+
+    while(1) { __asm __volatile("mov $2, %%eax":::"eax"); }
+}
